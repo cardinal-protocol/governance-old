68eb66446c0399467974d776558cabdb13ad8867
aleem-ahmed <w3st.io2021@gmail.com>
2022-06-19 11:31:53 -0400

    Add Interface for cvxRewardPool

---
+++ interface/ICVXRewardPool.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+// [IMPORT]
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+
+
+interface IcvxRewardPool {
+	function totalSupply() external view returns (uint256);
+
+	function balanceOf(address account) external view returns (uint256);
+
+	function extraRewardsLength() external view returns (uint256);
+
+	function addExtraReward(address _reward) external;
+
+	function clearExtraRewards() external;
+
+	function lastTimeRewardApplicable() external view returns (uint256);
+
+	function rewardPerToken() external view returns (uint256);
+
+	function earnedReward(address account) external view returns (uint256);
+
+	function earned(address account) external view returns (uint256);
+
+	function stake(uint256 _amount) external;
+
+	function stakeAll() external;
+
+	function stakeFor(address _for, uint256 _amount) external;
+
+	function withdraw(uint256 _amount, bool claim) external;
+
+	function withdrawAll(bool claim) external;
+
+	function getReward(address _account, bool _claimExtras, bool _stake) external;
+
+	function getReward(bool _stake) external;
+
+	function donate(uint256 _amount) external returns(bool);
+
+	function queueNewRewards(uint256 _rewards) external;
+}
