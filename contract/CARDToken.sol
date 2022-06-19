3f5b59dfcb5fba0c37ef92cc39a24c71c6116bf4
aleem-ahmed <w3st.io2021@gmail.com>
2022-06-19 12:59:13 -0400

    Cosmetic Changes

--- contract/CARDToken.sol
+++ contract/CARDToken.sol
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
 // [IMPORT]
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
