87c670f57705c435ac0da98fcb599a3276399bf3
aleem-ahmed <w3st.io2021@gmail.com>
2022-06-28 16:01:54 -0400

    Improved

--- generative-art-nft/RobotsVsAliensRobots.sol
+++ generative-art-nft/RobotsVsAliensRobots.sol
@@ -1,8 +1,8 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 
-// [IMPORT]
+/* [IMPORT] */
 // access
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 // token
@@ -13,7 +13,8 @@ import "@openzeppelin/contracts/utils/escrow/Escrow.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-// [MAIN-CONTRACT]
+
+/* [MAIN-CONTRACT] */
 contract RobotsVsAliensRobots is
 	AccessControlEnumerable,
 	ERC721Enumerable,
@@ -31,13 +32,12 @@ contract RobotsVsAliensRobots is
 	uint private _max;
 	address _wallet;
 	bool _openMint;
-	bool _openWhitelistMint;
 
 
 	mapping(address => bool) private whitelist;
 
 
-	// [CONSTRUCTOR]
+	/* [CONSTRUCTOR] */
 	constructor (
 		string memory name,
 		string memory symbol,
@@ -52,33 +52,35 @@ contract RobotsVsAliensRobots is
 		_max = max;
 		_wallet = wallet;
 		_openMint = false;
-		_openWhitelistMint = false;
 		
 		_setupRole(DEFAULT_ADMIN_ROLE, wallet);
 		_setupRole(DEFAULT_ADMIN_ROLE, admin);
 	}
 
 
-	// [OVERRIDE-REQUIRED-FUNCTIONS]
+	/* [OVERRIDE-REQUIRED-FUNCTIONS] */
 	// _burn
 	function _burn(uint256 tokenId) internal virtual override(ERC721, ERC721URIStorage) {
 		return ERC721URIStorage._burn(tokenId);
 	}
+
 	// tokenURI
 	function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
 		return ERC721URIStorage.tokenURI(tokenId);
 	}
+
 	// _beforeTokenTransfer
 	function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721, ERC721Enumerable) {
 		super._beforeTokenTransfer(from, to, tokenId);
 	}
+
 	// supportsInterface
 	function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControlEnumerable, ERC721, ERC721Enumerable) returns (bool) {
 		return super.supportsInterface(interfaceId);
 	}
 
 
-	// [CUSTOM-FUNCTIONS]
+	/* [CUSTOM-FUNCTIONS] */
 	function setBaseURI(string memory baseURI) public onlyOwner {
         _baseTokenURI = baseURI;
     }
@@ -89,34 +91,18 @@ contract RobotsVsAliensRobots is
 	}
 
 
-	function setTokenURI(uint256 tokenId, string memory _tokenURI) external {
-		require(
-			hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
-			"!auth"
-		);
-
+	function setTokenURI(uint256 tokenId, string memory _tokenURI) external onlyOwner {
 		_setTokenURI(tokenId, _tokenURI);
 	}
 
 
-	function setPrice(uint mintPrice) external {
-		require(
-			hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
-			"!auth"
-		);
-
+	function setPrice(uint mintPrice) external onlyOwner {
 		_price = mintPrice;
 	}
 
 
-	function setMint(bool openMint, bool openWhitelistMint) external {
-		require(
-			hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
-			"!auth"
-		);
-
+	function setMint(bool openMint) external onlyOwner {
 		_openMint = openMint;
-		_openWhitelistMint = openWhitelistMint;
 	}
 
 
@@ -126,22 +112,10 @@ contract RobotsVsAliensRobots is
 
 
 	function mint(address[] memory toSend) public payable onlyOwner {
-		require(
-			toSend.length <= 30,
-			"Max of 30 NFTs per mint"
-		);
-		require(
-			_openMint == true,
-			"Minting closed"
-		);
-		require(
-			msg.value == _price * toSend.length,
-			"Invalid msg.value"
-		);
-		require(
-			_tokenIdTracker.current() + toSend.length <= _max,
-			"Not enough NFTs left to be mint amount"
-		);
+		require(toSend.length <= 30, "Max of 30 NFTs per mint");
+		require(_openMint == true, "Minting closed");
+		require(msg.value == _price * toSend.length, "Invalid msg.value");
+		require(_tokenIdTracker.current() + toSend.length <= _max, "Not enough NFTs left to be mint amount");
 
 		// For each address to send to..
 		for (uint i = 0; i < toSend.length; i++) {
