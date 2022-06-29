ee068ae67c6f68193eaa69e3c9c8f7a458aa2326
aleem ahmed <w3st.io2021@gmail.com>
2022-06-29 18:17:12 -0400

    Improved

--- VSWorld/generative-art/generateArt.py
+++ VSWorld/generative-art/generateArt.py
@@ -1,5 +1,5 @@
-from PIL import Image
 import os
+from PIL import Image
 
 script_dir = os.path.dirname(__file__)
 
@@ -12,52 +12,43 @@ headTop = [
   os.path.join(script_dir, "traits/headTop/2.png")
 ]
 headVision = [
+  os.path.join(script_dir, "traits/headVision/1.png"),
 ]
 headSpeaker = [
+  os.path.join(script_dir, "traits/headSpeaker/1.png"),
 ]
 bodyType = [
+  os.path.join(script_dir, "traits/bodyType/1.png"),
 ]
 bodyComponent = [
+  os.path.join(script_dir, "traits/bodyComponent/1.png"),
 ]
 
 counter = 0
 
-def createImage(a, b, counter):
+def createImage(a, b, c, d, e, f, counter):
   image01 = Image.open(headType[a]).convert("RGBA")
   image02 = Image.open(headTop[b]).convert("RGBA")
-  '''
   image03 = Image.open(headVision[c]).convert("RGBA")
   image04 = Image.open(headSpeaker[d]).convert("RGBA")
   image05 = Image.open(bodyType[e]).convert("RGBA")
   image06 = Image.open(bodyComponent[f]).convert("RGBA")
-  '''
-
 
   intermediate = Image.alpha_composite(image01, image02)
-  '''
   intermediate2 = Image.alpha_composite(intermediate, image03)
   intermediate3 = Image.alpha_composite(intermediate2, image04)
   intermediate4 = Image.alpha_composite(intermediate3, image05)
   intermediate5 = Image.alpha_composite(intermediate4, image06)
-  '''
 
   name = os.path.join(script_dir, "result/" + str(counter) + ".png")
-  
-  intermediate.save(name)
 
+  intermediate5.save(name)
 
 for a in range(len(headType)):
   for b in range(len(headTop)):
-    createImage(a, b, counter)
-    counter = counter + 1
-
-'''
-for a in range(4):
-  for b in range(4):
-    for c in range(4):
-      for d in range(4):
-        for e in range(4):
-          for f in range(2):
-            createImage(a,b,c,d,e,f,counter)
-             counter = counter + 1
-'''    
+    for c in range(len(headVision)):
+      for d in range(len(headSpeaker)):
+        for e in range(len(bodyType)):
+          for f in range(len(bodyComponent)):
+            createImage(a, b, c, d, e, f, counter)
+            counter = counter + 1
