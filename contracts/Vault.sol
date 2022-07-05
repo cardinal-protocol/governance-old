224b3443841f2985f70c339657f46e4ec3bbb931
aleem ahmed <w3st.io2021@gmail.com>
2022-07-05 13:46:43 -0400

    Cosmetic

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -2,12 +2,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-
-
 contract Vault {
-	/* ========== [STATE VARIABLES Maps] ========== */
+	/* ========== STATE VARIABLES ========== */
+	
 	mapping(uint => bool) public active;
 
+    /* ========== MUTATIVE FUNCTIONS ========== */
+	
 	function deposit(uint strategyId, uint256 _amount) public {
 		require(active[strategyId], "Strategy is not active");
 
