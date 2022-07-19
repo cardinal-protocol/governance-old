59fed2bff93f5be027e49ba68b756f9a5bd2971e
aleem ahmed <w3st.io2021@gmail.com>
2022-07-19 11:41:39 -0400

    Make Uniswap code easier to read

--- UniswapV2Router02.sol
+++ UniswapV2Router02.sol
@@ -261,18 +261,27 @@ contract UniswapV2Router02 is IUniswapV2Router02 {
         if (IUniswapV2Factory(factory).getPair(tokenA, tokenB) == address(0)) {
             IUniswapV2Factory(factory).createPair(tokenA, tokenB);
         }
+
         (uint reserveA, uint reserveB) = UniswapV2Library.getReserves(factory, tokenA, tokenB);
+        
         if (reserveA == 0 && reserveB == 0) {
             (amountA, amountB) = (amountADesired, amountBDesired);
-        } else {
+        }
+        else {
             uint amountBOptimal = UniswapV2Library.quote(amountADesired, reserveA, reserveB);
+        
             if (amountBOptimal <= amountBDesired) {
                 require(amountBOptimal >= amountBMin, 'UniswapV2Router: INSUFFICIENT_B_AMOUNT');
+            
                 (amountA, amountB) = (amountADesired, amountBOptimal);
-            } else {
+            }
+            else {
                 uint amountAOptimal = UniswapV2Library.quote(amountBDesired, reserveB, reserveA);
+            
                 assert(amountAOptimal <= amountADesired);
+            
                 require(amountAOptimal >= amountAMin, 'UniswapV2Router: INSUFFICIENT_A_AMOUNT');
+            
                 (amountA, amountB) = (amountAOptimal, amountBDesired);
             }
         }
@@ -287,12 +296,24 @@ contract UniswapV2Router02 is IUniswapV2Router02 {
         address to,
         uint deadline
     ) external virtual override ensure(deadline) returns (uint amountA, uint amountB, uint liquidity) {
-        (amountA, amountB) = _addLiquidity(tokenA, tokenB, amountADesired, amountBDesired, amountAMin, amountBMin);
+        (amountA, amountB) = _addLiquidity(
+            tokenA,
+            tokenB,
+            amountADesired,
+            amountBDesired,
+            amountAMin,
+            amountBMin
+        );
+
         address pair = UniswapV2Library.pairFor(factory, tokenA, tokenB);
+        
         TransferHelper.safeTransferFrom(tokenA, msg.sender, pair, amountA);
         TransferHelper.safeTransferFrom(tokenB, msg.sender, pair, amountB);
+        
+        // Mint the tokens for the LP
         liquidity = IUniswapV2Pair(pair).mint(to);
     }
+    
     function addLiquidityETH(
         address token,
         uint amountTokenDesired,
