9ca607cda0bf897c5db20724ce72ecf606de095c
aleem ahmed <w3st.io2021@gmail.com>
2022-07-20 13:40:31 -0400

    Small Changes

--- contracts/StrategyX.sol
+++ contracts/StrategyX.sol
@@ -13,8 +13,10 @@ contract StrategyX {
 
 	/* ========== [STATE-VARIABLES] ========== */
 	
-	string _name = 'UNISWAP V2 DAI-USDC';
+	string _name = 'StrategyX - UniswapV2 DAI-USDC';
+	
 	bool active = false;
+
 	address[] _tokensUsed = [
 		0x6B175474E89094C44Da98b954EedeAC495271d0F,
 		0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
