def32ef153d5e5e91bb370479f1d136be5c38520
aleem ahmed <w3st.io2021@gmail.com>
2022-07-25 15:22:48 -0400

    Improved

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-etherscan');
@@ -7,14 +8,14 @@ require('@nomiclabs/hardhat-etherscan');
  */
 module.exports = {
   solidity: "0.8.10",
-  network: {
+  networks: {
     ropsten: {
       url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY]
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY]
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     },
   },
   etherscan: {
