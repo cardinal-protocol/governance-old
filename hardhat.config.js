47570d049bde6e216ee256a2251ec4e8a5345248
aleem-ahmed <w3st.io2021@gmail.com>
2022-07-25 10:12:32 -0400

    Improved

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 //require("@nomiclabs/hardhat-waffle");
 require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-etherscan');
-require('@openzeppelin/hardhat-etherscan');
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
