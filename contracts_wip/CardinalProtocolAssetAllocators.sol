dd457acabf0bae9903120aa0a0a20d743aae8cdc
aleem-ahmed <w3st.io2021@gmail.com>
2022-07-31 12:41:29 -0400

    Improved

--- contracts_wip/CardinalProtocolAssetAllocators.sol
+++ contracts_wip/CardinalProtocolAssetAllocators.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.9;
 
 
 /* ========== [IMPORT] ========== */
+// @openzeppelin/contracts/interfaces
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
 // @openzeppelin/contracts/token
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 // @openzeppelin/contracts/utils
@@ -31,30 +33,41 @@ contract CardinalProtocolAssetAllocators is
 	struct Guideline {
 		StrategyAllocation[] strategyAllocations;
 	}
+	
+
+	/* ========== [STATE VARIABLE][CONSTANT] ========== */
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
 
 	/* ========== [STATE VARIABLE] ========== */
 	// Custom Types
-	Counters.Counter public _tokenIdTracker;
+	Counters.Counter public _CPAATokenIdTracker;
 
 	string public _baseTokenURI;
 	address public _treasury;
 
+	// Strategy Id => Whitelisted Strategy
 	mapping(uint64 => address) _whitelistedStrategyAddresses;
+
+	// CPAA Id =>
+	mapping(uint256 => uint256) _WETHBalanceOf;
 	mapping(uint256 => Guideline) _guidelines;
 
 
 	/* ========== [EVENT] ========== */
 	event DepositedEther(
+		uint256 CPAATokenId,
 		uint256 amount
 	);
 
 	event DepositedTokensIntoStrategy(
+		uint256 CPAATokenId,
 		uint64 strategy,
 		uint256[] amounts
 	);
 
 	event WithdrewTokensFromStrategy(
+		uint256 CPAATokenId,
 		uint64 strategy
 	);
 
@@ -74,10 +87,10 @@ contract CardinalProtocolAssetAllocators is
 
 
 	/* ========== [MODIFIER] ========== */
-	modifier auth_ownsNFT(uint256 AssetAllocatorTokenId) {
+	modifier auth_ownsNFT(uint256 CPAATokenId) {
 		// Check if the wallet owns the assetAllocatorId
 		require(
-			ownerOf(AssetAllocatorTokenId) == msg.sender,
+			ownerOf(CPAATokenId) == msg.sender,
 			"You do not own this AssetAllocator token"
 		);
 
@@ -86,27 +99,28 @@ contract CardinalProtocolAssetAllocators is
 	
 	
 	/* ========== [OVERRIDE][FUNCTION][REQUIRED] ========== */
-	function _burn(uint256 tokenId) internal virtual override(ERC721) {
+	function _burn(uint256 CPAATokenId) internal virtual override(ERC721) {
 		// Distribute the tokens that are being yield farmed
 
-		for (uint256 i = 0; i < _guidelines[tokenId].strategyAllocations.length; i++) {
+		for (uint256 i = 0; i < _guidelines[CPAATokenId].strategyAllocations.length; i++) {
 			// Withdraw tokens
 
 			// [EMIT]
 			emit WithdrewTokensFromStrategy(
-				_guidelines[tokenId].strategyAllocations[i].id
+				CPAATokenId,
+				_guidelines[CPAATokenId].strategyAllocations[i].id
 			);
 		}
 
-		return ERC721._burn(tokenId);
+		return ERC721._burn(CPAATokenId);
 	}
 
 	// Return the full URI of a token
-	function tokenURI(uint256 tokenId) public view
+	function tokenURI(uint256 CPAATokenId) public view
 		override(ERC721)
 		returns (string memory)
 	{
-		return ERC721.tokenURI(tokenId);
+		return ERC721.tokenURI(CPAATokenId);
 	}
 
 
@@ -127,48 +141,51 @@ contract CardinalProtocolAssetAllocators is
 		// For each toSend, mint the NFT
 		for (uint i = 0; i < toSend.length; i++) {
 			// Mint token
-			_mint(toSend[i], _tokenIdTracker.current());
+			_mint(toSend[i], _CPAATokenIdTracker.current());
 
 			// Create GuideLine for minted token
-			_guidelines[_tokenIdTracker.current()] = guideline_;
+			_guidelines[_CPAATokenIdTracker.current()] = guideline_;
 			
 			// [INCREMENT] token id
-			_tokenIdTracker.increment();
+			_CPAATokenIdTracker.increment();
 		}
 	}
 
-	function depositEther(
-		uint256 tokenId,
-		uint256 amount_
-	) public
-		auth_ownsNFT(tokenId)
+	/// @notice Deposit WETH into this contract
+	function depositWETH(uint256 CPAATokenId, uint256 amount_) public payable
+		auth_ownsNFT(CPAATokenId)
 	{
-		// [EMIT]
-		emit DepositedEther(
+		IERC20(WETH).transferFrom(
+			msg.sender,
+			address(this),
 			amount_
 		);
 
-		// For each Strategy Allocation
-		for (uint i = 0; i < _guidelines[tokenId].strategyAllocations.length; i++) {
-			// Swap eth for 
+		// Increment _WETHBalanceOf
+		_WETHBalanceOf[CPAATokenId] = _WETHBalanceOf[CPAATokenId] + amount_;
 
-			// Deposit tokens into strategy
-		}
+		// [EMIT]
+		emit DepositedEther(CPAATokenId, amount_);
 	}
 
 	function depositTokensIntoStrategies(
-		uint256 tokenId,
+		uint256 CPAATokenId,
 		uint256[] memory amounts_
 	) public
-		auth_ownsNFT(tokenId)
+		auth_ownsNFT(CPAATokenId)
 	{
 		// For each Strategy Allocation
-		for (uint i = 0; i < _guidelines[tokenId].strategyAllocations.length; i++) {
+		for (
+			uint i = 0;
+			i < _guidelines[CPAATokenId].strategyAllocations.length;
+			i++
+		) {
 			// Deposit tokens
 			
 			// [EMIT]
 			emit DepositedTokensIntoStrategy(
-				_guidelines[tokenId].strategyAllocations[i].id,
+				CPAATokenId,
+				_guidelines[CPAATokenId].strategyAllocations[i].id,
 				amounts_
 			);
 		}
