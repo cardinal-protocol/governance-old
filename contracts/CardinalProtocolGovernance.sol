313320330ea876e47a58370f4cb46f22fcb4121b
harpoonjs <w3st.io2021@gmail.com>
2022-12-19 21:04:44 -0500

    Remove DEFAULT_ADMIN_ROLE

--- contracts/CardinalProtocolGovernance.sol
+++ contracts/CardinalProtocolGovernance.sol
@@ -38,57 +38,8 @@ contract CardinalProtocolGovernance is
 	/** [constructor] */
 	constructor ()
 	{
-		_setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-	}
+		_setRoleAdmin(DEFAULT_ADMIN_ROLE, S);
 
-	/** [public][view] */
-	/// @inheritdoc ICardinalProtocolGovernance
-	function hasClearanceAdmin(address a) public view returns (bool)
-	{
-		return
-			hasRole(DEFAULT_ADMIN_ROLE, a)
-		;
-	}
-
-	/// @inheritdoc ICardinalProtocolGovernance
-	function hasClearanceS(address a) public view returns (bool)
-	{
-		return
-			hasRole(DEFAULT_ADMIN_ROLE, a) ||
-			hasRole(S, a)
-		;
-	}
-
-	/// @inheritdoc ICardinalProtocolGovernance
-	function hasClearanceA(address a) public view returns (bool)
-	{
-		return
-			hasRole(DEFAULT_ADMIN_ROLE, a) ||
-			hasRole(S, a) ||
-			hasRole(A, a)
-		;
-	}
-
-	/// @inheritdoc ICardinalProtocolGovernance
-	function hasClearanceB(address a) public view returns (bool)
-	{
-		return
-			hasRole(DEFAULT_ADMIN_ROLE, a) ||
-			hasRole(S, a) ||
-			hasRole(A, a) ||
-			hasRole(B, a)
-		;
-	}
-
-	/// @inheritdoc ICardinalProtocolGovernance
-	function hasClearanceC(address a) public view returns (bool)
-	{
-		return
-			hasRole(DEFAULT_ADMIN_ROLE, a) ||
-			hasRole(S, a) ||
-			hasRole(A, a) ||
-			hasRole(B, a) ||
-			hasRole(C, a)
-		;
+		_setupRole(S, _msgSender());
 	}
 }
