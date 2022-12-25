8b163d2f3dc4ba7cd1c2deb839000d0030578133
harpoonjs <w3st.io2021@gmail.com>
2022-12-25 18:04:45 -0500

    Move logo, improve comments, and add credit

--- contracts/interface/ICardinalProtocolGovernance.sol
+++ contracts/interface/ICardinalProtocolGovernance.sol
@@ -8,25 +8,29 @@ pragma solidity ^0.8.10;
 interface ICardinalProtocolGovernance 
 {
 	/**
-	* @notice AccessControl Role
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
 	* @return {bytes32} keccak256 value
 	*/
 	function S() external view returns (bytes32);
 
 	/**
-	* @notice AccessControl Role
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
 	* @return {bytes32} keccak256 value
 	*/
 	function A() external view returns (bytes32);
 
 	/**
-	* @notice AccessControl Role
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
 	* @return {bytes32} keccak256 value
 	*/
 	function B() external view returns (bytes32);
 
 	/**
-	* @notice AccessControl Role
+	* @notice AccessControlEnumerable role
+	* @dev [view-bytes32]
 	* @return {bytes32} keccak256 value
 	*/
 	function C() external view returns (bytes32);
